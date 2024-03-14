5286716d1492e78e4ae2aee559c29d02d0855d0c
vikhulei <vikhulei@gmail.com>
2024-03-14 21:15:52 +0000

    questions added

--- src/Questions.js
+++ src/Questions.js
@@ -1,7 +1,10 @@
 const Questions = {
   ADDS: [
-    {q: 'Which command is used to check FSMO role?', a: 'netdom query fsmo', tag: '0'},
-    {q: 'What do you need to run to implement SSPR - self-service password reset in Azure AD?', a: 'Run Microsoft Azure AD Connect and select Password reset'},
+    {q: 'Which command is used to check FSMO role?', a: 'netdom query fsmo', tag: 0},
+    {q: 'What do you need to run to implement SSPR - self-service password reset in Azure AD?', a: 'Run Microsoft Azure AD Connect and select Password reset', tag: 0},
+    {q: 'Which group can manage GPOs (least priviledge)?', a: 'Domain Admins', tag: 0},
+    {q: 'Three steps to deploy AAD DS and prepare it for VM joining', a: '1) Create Az virtual network<br>2) Create Azure AD DS instance<br>3) Modify virtual network settings', tag: 0},
+    {q: '', a: '', tag: 0},
   ],
   
 
