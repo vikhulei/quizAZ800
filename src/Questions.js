d237c6ea6327a21824c18fbcc985b2096b022779
vikhulei <vikhulei@gmail.com>
2024-03-25 09:57:00 +0000

    all categories created, question added

--- src/Questions.js
+++ src/Questions.js
@@ -7,10 +7,22 @@ const Questions = {
     {q: 'Which tool is used to seize operations master roles?', a: 'ntdsutil.exe', tag: 0},
     {q: 'Which tool is used to trigger ADDS schema update?', a: 'ADSIEDIT.MSC', tag: 0},
     {q: 'Which folder are started GPOs stored at?', a: 'StartGPO in SYSVOL', tag: 0},
-    {q: '', a: '', tag: 0},
+    {q: 'How to set up replication between two sites?', a: '1) Create sites and move DCs to them<br>2) Create connection objects for the pair of DCs', tag: 0},
   ],
   Management: [
-    {q: 'What are two ways to connect Windiows Admin Center to Azure VM?', a: '1) Assign public IP to VM;<br>2) Use VPN', tag: 0},
+    {q: 'What are two ways to connect Windows Admin Center to Azure VM?', a: '1) Assign public IP to VM;<br>2) Use VPN', tag: 0},
+    {q: '', a: '', tag: 0},
+  ],
+  VMs_and_Containers: [
+    {q: 'Question about VMs', a: 'Answer about VMs', tag: 0},
+    {q: '', a: '', tag: 0},
+  ],
+  Network: [
+    {q: 'Question about network', a: 'Answer about network', tag: 0},
+    {q: '', a: '', tag: 0},
+  ],
+  Storage: [
+    {q: 'Question about Storage', a: 'Answer about storage', tag: 0},
     {q: '', a: '', tag: 0},
   ],
 
